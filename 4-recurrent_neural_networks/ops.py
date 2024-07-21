86a0fad4f136a0752fb5ded5d2bf8267c1356ca8
manuelmolano <manuelmolanomazon@gmail.com>
2024-07-21 12:20:25 +0200

    plotting behav of trained net

--- 4-recurrent_neural_networks/ops.py
+++ 4-recurrent_neural_networks/ops.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import numpy as np
 import matplotlib.pyplot as plt
 import torch.nn as nn
+from scipy.special import erf
 
 def get_modelpath(envid):
     """
@@ -17,14 +18,15 @@ def get_modelpath(envid):
     
     return path
 
-def plot_trials_timestep(inputs, actions, gt, perf, env_kwargs, reward=None):
+def plot_trials_timestep(inputs, actions, gt, perf, env_kwargs, num_steps=None, reward=None):
     inputs = np.array(inputs)
     actions = np.array(actions)
     gt = np.array(gt)
     perf = np.array(perf)
     reward = np.array(reward)   
     # Plot first 40 steps
-    num_steps = 20
+    if num_steps is None:
+        num_steps = len(gt)
     f, ax = plt.subplots(ncols=1, nrows=3, figsize=(8, 4), dpi=150, sharex=True)
 
     ax[0].plot(np.arange(1, num_steps+1)*env_kwargs['dt'], inputs[:num_steps, 0], label='Fixation')
@@ -54,17 +56,38 @@ def plot_trial_sequence(correct_choice, block):
     plt.legend()    
 
 
+def probit(x, beta, alpha):
+    """
+    Return probit function with parameters alpha and beta.
+
+    Parameters
+    ----------
+    x : float
+        independent variable.
+    beta : float
+        sensitiviy.
+    alpha : TYPE
+        bias term.
+
+    Returns
+    -------
+    probit : float
+        probit value for the given x, beta and alpha.
+
+    """
+    probit = 1/2*(1+erf((beta*x+alpha)/np.sqrt(2)))
+    return probit
 
 class efficieNet(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super(efficieNet, self).__init__()
 
-        # INSTRUCTION 1: build a recurrent neural network with a single recurrent layer and rectified linear units
+        # build a recurrent neural network with a single recurrent layer and rectified linear units
         self.vanilla = nn.RNN(input_size, hidden_size, nonlinearity='relu')
         self.linear = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
-        # INSTRUCTION 2: get the output of the network for a given input
+        # get the output of the network for a given input
         out, _ = self.vanilla(x)
         x = self.linear(out)
         return x, out
